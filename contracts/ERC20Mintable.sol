605a77117c40c4755ff4e52eb548aff2b8342033
Ananya <31894391+AnanyaSharmaPy@users.noreply.github.com>
2019-10-06 00:54:07 +0530

    Added constructor

--- contracts/ERC20Mintable.sol
+++ contracts/ERC20Mintable.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.5.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/roles/MinterRole.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20Detailed.sol";
 
 /**
  * @dev Extension of {ERC20} that adds a set of accounts with the {MinterRole},
@@ -9,7 +10,7 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
  *
  * At construction, the deployer of the contract is the only minter.
  */
-contract ERC20Mintable is ERC20, MinterRole {
+contract ERC20Mintable is ERC20, MinterRole, ERC20Detailed {
     /**
      * @dev See {ERC20-_mint}.
      *
@@ -17,6 +18,10 @@ contract ERC20Mintable is ERC20, MinterRole {
      *
      * - the caller must have the {MinterRole}.
      */
+     
+    constructor (string memory name, string memory symbol, uint8 decimals) public ERC20Detailed(name, symbol, decimals){
+    }
+    
     function mint(address account, uint256 amount) public onlyMinter returns (bool) {
         _mint(account, amount);
         return true;
